bff03cb1da6973e3c9c2dc19968a1fc8dacf180c
bhanupr4 <54998844+bhanupr4@users.noreply.github.com>
2024-05-20 00:22:42 +0530

    Update AT45DB.h

--- AT45DB.h
+++ AT45DB.h
@@ -7,35 +7,24 @@
 
 #define DF_NR_PAGES     (1 << DF_PAGE_ADDR_BITS)
 
-void init(uint8_t csPin=SS);
-void init(uint8_t misoPin, uint8_t mosiPin, uint8_t sckPin, uint8_t ssPin) __attribute__((deprecated("Use: void init(uint8_t csPin=SS)")));
-void readID(uint8_t *data);
-void readSecurityReg(uint8_t *data, size_t size);
-
-uint8_t readByteBuf1(uint16_t pageAddr);
-void readStrBuf1(uint16_t addr, uint8_t *data, size_t size);
+void AT45DB_init(void);
+void en_tx(void);
+void dis_tx(void);
+uint8_t tx(uint8_t data);
+uint8_t AT45DB_status(void);
+void AT45DB_wait(void);
+void AT45DB_id(uint8_t *id);
+void getSecurity(uint8_t *data, size_t size);
+uint8_t getPageAddrB0(uint16_t pageAddr);
+uint8_t getPageAddrB1(uint16_t page);
+void setPageAddr(uint16_t pageAddr);
+void readPageToBuf1(uint16_t pageAddr);
+uint8_t readByteBuf1(uint16_t addr);
+void readBytesBuf1(uint16_t addr, uint8_t *data, size_t size);
 void writeByteBuf1(uint16_t addr, uint8_t data);
 void writeStrBuf1(uint16_t addr, uint8_t *data, size_t size);
-
 void writeBuf1ToPage(uint16_t pageAddr);
-void readPageToBuf1(uint16_t PageAdr);
-
-void pageErase(uint16_t pageAddr);
-void chipErase();
-
-void settings(SPISettings settings);
-
-
-uint8_t readStatus();
-void waitTillReady();
-uint8_t transmit(uint8_t data);
-void activate();
-void deactivate();
-void setPageAddr(unsigned int PageAdr);
-uint8_t getPageAddrByte0(uint16_t pageAddr);
-uint8_t getPageAddrByte1(uint16_t pageAddr);
-uint8_t getPageAddrByte2(uint16_t pageAddr);
-
-
+void page_Erase(uint16_t pageAddr);
+void chip_Erase();
 
 #endif // AT45DB_H
