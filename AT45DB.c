a744e75b7a382e1f4ea28b0a1492337fd7767072
bhanupr4 <54998844+bhanupr4@users.noreply.github.com>
2024-05-19 22:38:30 +0530

    Update AT45DB.c

--- AT45DB.c
+++ AT45DB.c
@@ -1,3 +1,19 @@
 //include necessary ASF headers
 #include <AT45DB.h>
 
+//Dataflash commands
+#define FlashPageRead           0xD2    // Main memory page read
+#define StatusReg               0xD7    // Status register
+#define ReadMfgID               0x9F    // Read Manufacturer and Device ID
+#define PageErase               0x81    // Page erase
+#define ReadSecReg              0x77    // Read Security Register
+
+#define FlashToBuf1Transfer     0x53    // Main memory page to buffer 1 transfer
+#define Buf1Read                0xD4    // Buffer 1 read
+#define Buf1ToFlashWE           0x83    // Buffer 1 to main memory page program with built-in erase
+#define Buf1Write               0x84    // Buffer 1 write
+
+#define FlashToBuf2Transfer     0x55    // Main memory page to buffer 2 transfer
+#define Buf2Read                0xD6    // Buffer 2 read
+#define Buf2ToFlashWE           0x86    // Buffer 2 to main memory page program with built-in erase
+#define Buf2Write               0x87    // Buffer 2 write
